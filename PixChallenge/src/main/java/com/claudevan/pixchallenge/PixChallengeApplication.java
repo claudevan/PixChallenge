9066db0ca94c53ee231661bd3985ad35d3b88b49
Claudevan <claudevan.ti@gmail.com>
2024-05-21 21:28:12 -0300

    chore: Inicio do projeto

---
+++ PixChallenge/src/main/java/com/claudevan/pixchallenge/PixChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.claudevan.pixchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixChallengeApplication.class, args);
+	}
+
+}
